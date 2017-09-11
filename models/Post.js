2ad3ba5dc0175fbbace4094f53eaeda3094fdfdf
dratech <alfonsotech@nym.hush.com>
2017-09-11 13:22:30 -0700

    Add MongoDB/Mongoose database and basic scraper funcitonality

---
+++ models/Post.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  }
+});
+
+var Post = mongoose.model("Post", PostSchema);
+
+module.exports = Post;
